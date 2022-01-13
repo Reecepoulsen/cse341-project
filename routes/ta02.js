4883f8e6874fee90caa50fbd6ec85a6d54ae60ba
Reecepoulsen <dreecepoulsen@gmail.com>
2022-01-13 12:23:05 -0700

    completed both stretch challenges

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,10 +1,9 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
 
 let userList = [];
-let displayError = false
+let displayError = false;
+let errorMessage = '';
 
 router.get('/', (req, res, next) => {
   res.render('pages/ta02', {
@@ -13,30 +12,41 @@ router.get('/', (req, res, next) => {
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
     users: userList, 
-    error: displayError
+    error: {display: displayError, message: errorMessage}
   });
 });
 
+const userExists = (username) => {
+  for (let user of userList) {
+    if (user.name == username) {
+      return true
+    };
+  }
+  return false;
+}
+
 router.post('/addUser', (req, res, next) => {
-  userList.push({name: req.body.username});
-  res.redirect('/ta02');
+  let username = req.body.username;
+  if (userExists(username)){
+    displayError = true;
+    errorMessage = `'${username} Already Exists'`
+    res.redirect('/ta02');
+  } else {
+    displayError = false;
+    userList.push({name: username});
+    res.redirect('/ta02');
+  }
 });
 
 router.post('/removeUser', (req, res, next) => {
   let username = req.body.removeName;
-  let found = false;
-  for (let user of userList) {
-    if (user.name == username) {
-      found = true
-    }
-  }
-
-  if (found) {
+  if (userExists(username)) {
     displayError = false;
-    userList = userList.filter((user) => user.name != req.body.removeName)
+    userList = userList.filter((user) => user.name != username)
     res.redirect('/ta02');
   } else {
     displayError = true;
+    errorMessage = `'${username}' Does Not Exist`;
     res.redirect('/ta02');
   }
 });
