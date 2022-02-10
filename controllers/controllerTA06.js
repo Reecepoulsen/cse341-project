6511eb8054cfdec0e4553191097b59297a296270
Reecepoulsen <dreecepoulsen@gmail.com>
2022-02-10 10:36:57 -0700

    halfway through ta06

--- controllers/controllerTA06.js
+++ controllers/controllerTA06.js
@@ -4,3 +4,25 @@ exports.getIndex = (req, res, next) => {
     path: '/ta06'
   })
 }
+
+exports.getRegister = (req, res, next) => {
+  res.render('pages/register', {
+    title: "Create Account",
+    path: '/ta06/register'
+  })
+}
+
+exports.getLogin = (req, res, next) => {
+  res.render('pages/register', {
+    title: "Login",
+    path: '/ta06/login'
+  })
+}
+
+exports.postRegister = (req, res, next) => {
+  
+}
+
+exports.postLogin = (req, res, next) => {
+  
+}
--- routes/ta06.js
+++ routes/ta06.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const controllerTA06 = require('../controllers/controllerTA06');
 
-router.get("/", controllerTA06.getIndex)
+router.get("/", controllerTA06.getIndex);
+
+router.get('/register', controllerTA06.getRegister);
+
+router.get('/login', controllerTA06.getLogin);
+
+router.post('/register', controllerTA06.postRegister);
+
+router.post('/login', controllerTA06.postLogin);
 
 module.exports = router;
