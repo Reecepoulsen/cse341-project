775bc733bbfa1e71dc87c1eeff68983d572dd6f7
Reecepoulsen <dreecepoulsen@gmail.com>
2022-02-11 09:28:30 -0700

    finished w06 readings

--- controllers/controllerTA06.js
+++ controllers/controllerTA06.js
@@ -1,28 +1,105 @@
+const fs = require('fs');
+const { validationResult } = require("express-validator");
+
+
 exports.getIndex = (req, res, next) => {
   res.render('pages/ta06', {
     title: "Team Activity 06",
-    path: '/ta06'
+    path: '/ta06',
+    loggedIn: req.session.loggedIn
   })
 }
 
 exports.getRegister = (req, res, next) => {
   res.render('pages/register', {
     title: "Create Account",
-    path: '/ta06/register'
+    path: '/ta06/register',
+    errorMessage: null
   })
 }
 
 exports.getLogin = (req, res, next) => {
-  res.render('pages/register', {
+  res.render('pages/login', {
     title: "Login",
-    path: '/ta06/login'
+    path: '/ta06/login',
+    errorMessage: null
   })
 }
 
 exports.postRegister = (req, res, next) => {
-  
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('pages/register', {
+      title: "Create Account",
+      path: '/ta06/register',
+      errorMessage: errors.array()[0].msg
+    })
+  }
+
+  if (password !== confirmPassword) {
+    return res.redirect("/ta06/register");
+  }
+
+  const user = {
+    email: email,
+    password: password,
+  };
+
+  fs.writeFile('users.json', JSON.stringify(user), err => {
+    if (err) {
+      console.error(err);
+      return res.redirect("/ta06/register").status(403);
+    }
+  })
+  return res.redirect('/ta06/login');
 }
 
 exports.postLogin = (req, res, next) => {
-  
+  const email = req.body.email;
+  const password = req.body.password;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(403).render('pages/ta06', {
+      title: "Login",
+      path: "/ta06/login",
+      errorMessage: errors.array()[0].msg
+    })
+  }
+
+  fs.readFile('users.json', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/ta06/login").status(403);
+    }
+    user = JSON.parse(data);
+
+    if (user.email.toString() === email.toString() && user.password.toString() === password.toString()) {
+      req.session.loggedIn = true;
+      return res.render('pages/ta06', {
+        title: 'Team Activity 06',
+        path: '/ta06',
+        loggedIn: req.session.loggedIn
+      })
+    } 
+
+    return res.status(403).render("pages/login", {
+      title: "Login",
+      path: "ta06/login",
+      errorMessage: null
+    })
+  })
+}
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("logout error", err);
+    }
+    res.redirect("/ta06");
+  });
 }
--- routes/ta06.js
+++ routes/ta06.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
-const controllerTA06 = require('../controllers/controllerTA06');
+const { check } = require("express-validator");
+
+const controllerTA06 = require("../controllers/controllerTA06");
 
 router.get("/", controllerTA06.getIndex);
 
-router.get('/register', controllerTA06.getRegister);
+router.get("/register", controllerTA06.getRegister);
 
-router.get('/login', controllerTA06.getLogin);
+router.get("/login", controllerTA06.getLogin);
 
-router.post('/register', controllerTA06.postRegister);
+router.post(
+  "/register",
+  [
+    check("email", "Please enter a valid email")
+      .isEmail()
+      .normalizeEmail()
+      .trim(),
+    check(
+      "password",
+      "Password has to 5 characters long and include only letters or numbers."
+    )
+      .isAlphanumeric()
+      .isLength({ min: 5 })
+      .trim(),
+  ],
+  controllerTA06.postRegister
+);
 
-router.post('/login', controllerTA06.postLogin);
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email")
+      .isEmail()
+      .normalizeEmail()
+      .trim(),
+    check("password", "Incorrect password")
+      .isAlphanumeric()
+      .isLength({ min: 5 })
+      .trim(),
+  ],
+  controllerTA06.postLogin
+);
+
+router.post("/logout", controllerTA06.postLogout);
 
 module.exports = router;
